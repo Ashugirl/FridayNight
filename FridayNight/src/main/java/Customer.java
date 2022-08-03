7201450622fa1038e993919a3a7df37b520bf029
Aviva Shuman <97950104+Ashugirl@users.noreply.github.com>
2022-08-03 13:27:15 +0200

    Add files via upload

---
+++ FridayNight/src/main/java/Customer.java
@@ -0,0 +1,93 @@
+public class Customer {
+
+    private String name;
+    private int age;
+    private double euros;
+    private boolean hasCondom;
+    private int beersThatTheyDrunk;
+    private final double tippingAmount = 1.50;
+
+    public Customer() {
+    }
+
+    public Customer(String name, int age, double euros, boolean hasCondom) {
+        this.name = name;
+        this.age = age;
+        this.euros = euros;
+      
+        this.hasCondom = hasCondom;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getEuros() {
+        return euros;
+    }
+
+    public void setEuros(double euros) {
+        this.euros = euros;
+    }
+
+
+
+    public boolean isHasCondom() {
+        return hasCondom;
+    }
+
+    public void setHasCondom(boolean hasCondom) {
+        this.hasCondom = hasCondom;
+    }
+
+    public int getBeersThatTheyDrunk() {
+        return beersThatTheyDrunk;
+    }
+
+    public void setBeersThatTheyDrunk(int beersThatTheyDrunk) {
+        this.beersThatTheyDrunk = beersThatTheyDrunk;
+    }
+
+
+    public boolean feelingConfident(){
+        //check if they drank more than 5 beers and if they have a condom
+        if(getBeersThatTheyDrunk() > 5 && isHasCondom()){
+            return true;
+        } return false;
+    }
+
+    public int tipTheCuteStaff(Staff staff){
+        // check how many drinks you had.
+        //multiply that amount of drinks with the tippingAmount
+        //check if you have enough money. If the amount is too low,
+        //only give the amount that you have left
+        //return the amount of money you have left
+        int idealTip = (int) (getBeersThatTheyDrunk()*tippingAmount);
+        int actualTip = 0;
+
+            if(getEuros() > idealTip){
+                 actualTip = idealTip;
+                setEuros(getEuros()-actualTip);
+                staff.setAmountsOfTips(actualTip + staff.getAmountsOfTips());   return (int)euros;
+            }
+            actualTip = (int) getEuros();
+            setEuros(getEuros()-actualTip);
+
+        staff.setAmountsOfTips(actualTip + staff.getAmountsOfTips());
+        return (int) getEuros();
+
+
+    }
+}
